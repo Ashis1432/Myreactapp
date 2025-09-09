9a01a76436546fcf7a36359ee42e418415ffefa5
Ashis1432 <ashispanda1432@gmail.com>
2025-09-09 14:49:38 +0530

    reactapp done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export const App = () =>{
+  return( <div>
+    <div>
+      <img src="ot.jpg" alt="ot.jpg" height="40%" width="40%" />
+    </div>
+    <h2>Name : "Queen Of Tears"</h2>
+  <h3>Rating: 8.2</h3>
+  <p>Summery : Lorem ipsum, dolor sit amet consectetur 
+    adipisicing elit. Sit, ut nisi suscipit porro reiciendis,
+     corporis sed maiores, magni dolorem
+     dolores eos voluptatem aliquam iste quisquam 
+     tempora quae vero error laboriosam?</p>
+     </div>
+  )
+}
